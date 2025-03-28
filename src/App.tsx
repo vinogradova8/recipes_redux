f0a0c9521398030cbe94e3681c5c1d299c9342fe
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2025-03-28 13:45:59 +0200

    added redux to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
 import React from 'react';
 import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { Outlet } from 'react-router-dom';
 
 export const App: React.FC = () => {
-  return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
-  );
+  return <Outlet />;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,37 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HomePage } from './components/HomePage';
+import { RecipeDetailsPage } from './components/RecipeDetailsPage';
+import { Cart } from './components/Cart';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from 'react-redux';
+import store from './store';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<App />}></Route>
+          <Route index element={<HomePage />}></Route>
+          <Route path="/recipe/:itemId" element={<RecipeDetailsPage />} />
+          <Route path="/recipe/cart" element={<Cart />} />
+          <Route
+            path="*"
+            element={
+              <h3 style={{ padding: 30 }} className="title">
+                Page not found
+              </h3>
+            }
+          ></Route>
+        </Routes>
+      </HashRouter>
+
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  </Provider>,
+);
